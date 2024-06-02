a2d2b3a529514826225821f601a008827901c5ff
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2024-06-02 18:39:20 +0000

    done

--- 01_practice/datatype_conv.js
+++ 01_practice/datatype_conv.js
@@ -1,13 +1,80 @@
-//  let score = 33
-// let score = "33"
-// let score = ""
-// let score = null
-// let score = undefined
-// let score = undefined
-// console.log(score)
+// let score ="33";
+// let score ="33abc";
+// let valueInNumber = Number(score)
 // console.log(typeof score);
-// console.log(typeof (score));
-let score = "33"
-let valueInNumber = Number(score)
-console.log(valueInNumber);
-console.log(typeof valueInNumber );
+// console.log(typeof(score));
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+ //****string*******to*******number******* */
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+// same as check number to string
+
+//*****Null******* */
+// let score = null
+// console.log(typeof(score));
+// let valueInNumber = Number(null)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+//*****undefined******* */
+// let score = undefined
+// letvalueInNumber = Number(score)
+// console.log(score);
+//  console.log(typeof (score));
+// console.log(letvalueInNumber);
+// console.log(typeof letvalueInNumber);
+
+//*****boolean******* */
+
+// let score = true
+
+
+// console.log(score);
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+//  let score = false
+//  console.log(score);
+//  console.log(typeof score);
+//  let valueInNumber = Number(score);
+//  console.log(valueInNumber);
+//  console.log(typeof valueInNumber);
+
+//*****Name******* */
+// let score = "madhu";
+// console.log(score);
+// console.log(typeof score);
+// let valueInNumber = number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+ 
+
+//"33" => 33
+//"33abc" =>  not a number(NaN)
+//true => 1;
+// false =>0;
+//  **********************************************
+// let isLoggedIn = 1
+
+// console.log(isLoggedIn);
+// console.log(typeof isLoggedIn);
+// let booleanisLoggedIn = Boolean(isLoggedIn) 
+// console.log(booleanisLoggedIn);
+// console.log(typeof booleanisLoggedIn);
+
+// let isLoggedIn = ""
+// console.log(isLoggedIn);
+// console.log(typeof isLoggedIn);
+// let boolienisLoggedIn = Boolean(isLoggedIn)
+// console.log(boolienisLoggedIn);
+// console.log(typeof boolienisLoggedIn);
+
+// let isLoggedIn = "madhu"
+// console.log(isLoggedIn);
+// console.log(typeof isLoggedIn);
