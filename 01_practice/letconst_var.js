e032b9192ec4dc230756468835740472fb9e2fdf
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2024-06-02 06:00:25 +0000

    done1

---
+++ 01_practice/letconst_var.js
@@ -0,0 +1,16 @@
+const accountId = 123456;
+let emailId = "Madhu@gmail.com";
+var password = "234565";
+accountCity = "Jaipur";
+let state;
+// accountId = 334455;
+// console.log(accountId);
+//console.log(emailId);
+
+// console.table([accountId,emailId,password,accountCity])
+// accountId = 1111122;
+emailId = "dollon@gmail.com";
+password = 33224423;
+accountCity = "banglore";
+console.table([accountId,emailId,password,accountCity,state])
+// accountId = 1111122;
