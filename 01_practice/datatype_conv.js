643c5e947f6844c3540b5f4814a63b1dfa60fea1
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2024-06-02 15:32:40 +0000

    datatypes done

---
+++ 01_practice/datatype_conv.js
@@ -0,0 +1,13 @@
+//  let score = 33
+// let score = "33"
+// let score = ""
+// let score = null
+// let score = undefined
+// let score = undefined
+// console.log(score)
+// console.log(typeof score);
+// console.log(typeof (score));
+let score = "33"
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber );
