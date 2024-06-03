66607c876fd14e344e55356991da24525fa725b0
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2024-06-03 03:13:44 +0000

    done

--- 01_practice/datatype_conv.js
+++ 01_practice/datatype_conv.js
@@ -84,3 +84,46 @@
 // let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// **************************Operations*****************************
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "Hello"
+// let str2 = "Madhu"
+
+// let str3 =  str1 +  str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 +  "2");
+//https://tc39.es/ecma262/2023/#sec-toprimitive
+
+// console.log(3 + 4 * 5 % 3); not perfect code
+// console.log((3+4) *5 % 3);
+
+// console.log(true);
+// console.log(true +);
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3
+// num1 = num2 = num3 = 2+2 // not perfect
+// console.log(num1 = num2 = num3 = 2+2);
+
+// let gameCounter = 100
+// gameCounter++
+// ++gameCounter
+// console.log(gameCounter);
+
+// study --- prefix and postfix js mdn
+
+
