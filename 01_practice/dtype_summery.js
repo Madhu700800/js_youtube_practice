5b566387332cfb6d8ad67bd418e62dd10fff32d5
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2024-06-08 12:35:33 +0000

    summery1

---
+++ 01_practice/dtype_summery.js
@@ -0,0 +1,26 @@
+// Primitive 
+
+// 7 types :- String, Number, Boolean,Null,Undefined,Symbol,Bigint
+
+
+
+// const score = 100;
+// const scoreValue = 100.2;
+
+//  const isLoggedINn = false;
+//  let outsideTemp = null;
+// console.log(typeof outsideTemp);
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+// const bigNum = 3435367654879875676583892746n
+// console.log(typeof bigNum);
+
+//Reference(Non Primitive)
+
+// Array, Object,Boolean
+
+
