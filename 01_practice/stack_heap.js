344b025462084e8177b5ef19a6e1ccd21af5ca35
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2024-06-09 06:11:48 +0000

    stackandheap done

---
+++ 01_practice/stack_heap.js
@@ -0,0 +1,20 @@
+// stack
+// let myYoutubename = "MadhuAlwaysSweetdotcom"
+// let anotherName = myYoutubename
+// // console.log(anotherName);
+// anotherName = "Madhu got job ,his salary 35000 per months"
+// console.log(myYoutubename);
+// console.log(anotherName);
+
+let userOne = {
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+// console.log(userTwo);
+userTwo.email = "Madhu@google.com";
+userTwo.upi ="gpau@ybl";
+console.log(userOne);
+console.log(userTwo);
