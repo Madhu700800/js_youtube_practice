8f6f4438efa1c5a5d2b1fd126844c7f82724f1b9
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2024-06-13 08:18:59 +0000

    stringpractice

---
+++ 01_practice/string_javascript.js
@@ -0,0 +1,41 @@
+let name = "Madhu";
+let repoCount = 3;
+
+// console.log(`Hello ${name} and repocount is ${repoCount}`);
+
+let gameName = new String("Madhumc")
+
+//key access
+// console.log(gameName[0]);
+//toUppercase
+// console.log(gameName.toUpperCase());
+//split
+// console.log(gameName.split("mc"));
+//proto
+// console.log(gameName.__proto__);
+//length
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('d'));
+// let gameName1 = new String("Madhu_mc")
+// let newsubString = gameName1.substring(2,6)
+// let newsubString = gameName1.substring(1,3)
+// console.log(newsubString);
+// let anotherString = gameName1.slice(0,4)
+// const anotherString = gameName1.slice(7 ,6)
+
+// console.log(anotherString);
+// trim
+// let newstring = "  madhu  ";
+// console.log(newstring);
+// console.log(newstring.trim());
+//goto google search = trim javascript mdn
+
+// let url = "https://madhu.com/madhu%20rout";
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('madhu'));
+// //convert array 
+// let myName = new String('Madhu_priya-rout');
+// console.log(myName.split('-'));
+// let myName = new String('Madhu + priya + rout');
+// console.log(myName.split('+'));
